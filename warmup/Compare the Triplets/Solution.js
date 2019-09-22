5d9bb59718c8ee87f36f4d1bf4b8b133db948ded
ZALYAUL Mohammed Rabie <mrzalyaul@gmail.com>
2019-09-22 12:58:10 +0100

    Compare the Triplets

---
+++ warmup/Compare the Triplets/Solution.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+
+    let n = a.length;
+    let alicePoints = 0, bobPoints = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (a[i] > b[i]) {
+            alicePoints += 1;
+        } else if (a[i] < b[i]) {
+            bobPoints += 1;
+        }
+    }
+
+    return [alicePoints, bobPoints];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
