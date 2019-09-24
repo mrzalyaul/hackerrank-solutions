2b237e4622642b4276a539b0e0f4247fd2b21cdd
ZALYAUL Mohammed Rabie <mrzalyaul@gmail.com>
2019-09-24 11:50:02 +0100

    Bon Appétit

---
+++ Implementation/Bon/Solution.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the bonAppetit function below.
+function bonAppetit(bill, k, b) {
+
+    let bactual = bill.reduce((pre, cur, i) => k != i ? pre + cur : pre, 0);
+
+    bactual /= 2;
+
+    if (b === bactual) {
+        console.log('Bon Appetit');
+    } else {
+        console.log(b - bactual)
+    }
+
+}
+
+function main() {
+    const nk = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const bill = readLine().replace(/\s+$/g, '').split(' ').map(billTemp => parseInt(billTemp, 10));
+
+    const b = parseInt(readLine().trim(), 10);
+
+    bonAppetit(bill, k, b);
+}
